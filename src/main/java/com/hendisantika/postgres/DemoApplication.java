b7a41c018f7652e83d9ad1eca8a686aba91c3a68
Hendi Santika <hendisantika@gmail.com>
2018-09-24 18:53:52 +0700

    #9 - Add Resource Not Found Exception

--- src/main/java/com/hendisantika/postgres/DemoApplication.java
+++ src/main/java/com/hendisantika/postgres/DemoApplication.java
@@ -2,10 +2,10 @@ package com.hendisantika.postgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@RestController
+@EnableJpaAuditing
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/hendisantika/postgres/exception/ResourceNotFoundException.java
@@ -0,0 +1,25 @@
+package com.hendisantika.postgres.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/09/18
+ * Time: 18.53
+ * To change this template use File | Settings | File Templates.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
